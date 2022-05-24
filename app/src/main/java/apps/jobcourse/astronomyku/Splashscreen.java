e62b0504cb2fd626805aa4875f7c57b2f7c93dc6
eksamsia <ekasamsiati03@mail.ugm.ac.id>
2022-05-24 15:22:39 +0700

    eksam's first commit

---
+++ app/src/main/java/apps/jobcourse/astronomyku/Splashscreen.java
@@ -0,0 +1,16 @@
+package apps.jobcourse.astronomyku;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
